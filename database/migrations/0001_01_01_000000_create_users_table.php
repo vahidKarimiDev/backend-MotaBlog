3ffa979cadce12f6ef2cf7a454fa1595d28c0ddb
vahid karimi <149614062+vahidKarimiDev@users.noreply.github.com>
2024-08-30 12:43:38 +0330

    create custom command artisan make:service

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('userName');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone')->unique();
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
 
