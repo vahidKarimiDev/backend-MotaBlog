72a20335f02fda37acc3a9470b4ff3da6b96034d
vahid karimi <149614062+vahidKarimiDev@users.noreply.github.com>
2024-08-31 17:04:39 +0330

    update migration posts and categories

--- database/migrations/2024_08_31_133308_create_posts_table.php
+++ database/migrations/2024_08_31_133308_create_posts_table.php
@@ -4,19 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('post', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string("title");
             $table->text("description");
             $table->string("slug")->unique();
             $table->json("photos");
-            $table->foreignId("categories_id")->constrained(table: 'category', indexName: 'id');
+            $table->foreignId("categories_id")->constrained(table: 'categories');
             $table->foreignId("admin_id")->constrained(table: "admin", indexName: "id")->cascadeOnDelete();
             $table->boolean("status")->default(0);
             $table->timestamps();
@@ -28,6 +29,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('post');
+        Schema::dropIfExists('posts');
     }
 };
